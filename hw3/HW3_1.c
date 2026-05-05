3d4cf02ffc3fded4ddaa780e1eae96e24cad0edf
dacheng <william050218@gmail.com>
2026-05-05 10:30:00 +0800

    update

--- hw3/HW3_1.c
+++ hw3/HW3_1.c
@@ -5,6 +5,7 @@
 #include<liburing.h>
 #include<sys/time.h>
 #include<sys/stat.h>
+#include<type.h>
 
 #define ENTRY 8
 #define BLOCK_SIZE 4096
@@ -42,5 +43,36 @@ void blocking_io(const char *src, const char * dest){
 }
 
 void async_io(const char *src, const char *dest){
-    
+    struct io_uring ring;
+    struct io_uring_sqe *sqe;
+    struct io_uring_cqe *cqe;
+    struct stat st;
+    int src_fd, dest_fd;
+    size_t file_size, num_block;
+
+    io_uring_queue_init(ENTRY, &ring, 0);
+    src_fd = open(src, O_RDONLY);
+    if(src_fd == -1){
+        perror("can't open source file.");
+        return;
+    }
+    dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC);
+    if(dest == -1){
+        perror("can't open destination file.");
+        return;
+    }
+
+    if (fstat(fd, &st) < 0) {
+    perror("fstat failed");
+    close(fd);
+    return;
+    }
+    file_size = st.size;
+    num_block = (file_size + BLOCK_SIZE) / BLOCK_SIZE;
+
+    for(int i = 0 ; i < num_block ; i++){
+        io_uring_get_sqe(&ring);
+        perror("can't get SQE\n");
+        return;
+    }
 }
